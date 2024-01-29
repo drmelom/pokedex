774e60e1ef62eedc8db66f2aac7c0384067992cd
David Melo <gmkronox@gmail.com>
2024-01-29 17:54:41 -0500

    Update configuration and logging

---
+++ src/config/app.config.ts
@@ -0,0 +1,8 @@
+export function EnvConfiguration() {
+  return {
+    enviroment: process.env.NODE_ENV || 'development',
+    mongodb: process.env.MONGODB,
+    port: process.env.PORT || 3000,
+    defaultLimit: +process.env.DEFAULT_LIMIT || 5,
+  };
+}
